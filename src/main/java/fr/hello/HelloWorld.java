74b1e4c0a5722980f120a7d62b328b8fdfca0892
PiA <77051645+palexforlodou@users.noreply.github.com>
2023-02-24 12:53:27 +0100

    Update HelloWorld.java

--- src/main/java/fr/hello/HelloWorld.java
+++ src/main/java/fr/hello/HelloWorld.java
@@ -13,10 +13,11 @@ public class HelloWorld {
 		 */
 		
 		String message = "Hello Maven World";
-		/**Afficher A = new Afficher( );
+		*Afficher A = new Afficher( );
 		A.display(message) ;
+		
+		/**System.out.println(message);
 		*/
-		System.out.println(message);
 
 	}
 
